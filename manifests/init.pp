ead1c8903522300f64238dcb541914fbcb534dcb
Michael Avila <me@michaelavila.com>
2015-12-28 23:17:25 -0800

    Upgrade to TOR 5.0.6

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,10 @@
 #
 #   include tor
 class tor (
-  $version = '4.0.1'
+  $version = '5.0.6'
 ) {
   package { 'TorBrowser':
     provider => 'appdmg',
-    source   => "https://dist.torproject.org/torbrowser/${version}/TorBrowser-${version}-osx32_en-US.dmg"
+    source   => "https://dist.torproject.org/torbrowser/${version}/TorBrowser-${version}-osx64_en-US.dmg"
   }
 }
